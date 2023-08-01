42d4dde10d50872ebf1d265ccaa621940dfd8f06
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-01 12:40:03 -0400

    added variables

--- variables.js
+++ variables.js
@@ -1,15 +1,18 @@
 var one = 'hello';
-
 var two = 'goodbye';
-
 var three = 'hellogoodbye';
 
 var number1 = 12;
-
 var number2 = 54;
-
 var number3 = 73;
 
 var boolean1 = true;
-
 var boolean2 = false;
+
+one = 'what';
+two = 'zebra';
+three = 'pig';
+
+console.log(one);
+console.log(two);
+console.log(three);
